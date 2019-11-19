58d08bda28e0f51ad2ac6e57ed57b56fd3ae5c98
MinexAutomation <minex.automation@gmail.com>
2019-11-18 21:29:41 -0800

    Removed GetRepositoryUrl() method.

--- source/R5T.Goteborg.Base/Code/Services/Interfaces/IGitHubOperator.cs
+++ source/R5T.Goteborg.Base/Code/Services/Interfaces/IGitHubOperator.cs
@@ -6,6 +6,6 @@ namespace R5T.Goteborg
     public interface IGitHubOperator
     {
         void CreateRepository(GitHubRepository repository);
-        string GetRepositoryUrl(string repositoryName);
+        //string GetRepositoryUrl(string repositoryName); // This does not require a GitHub! This can be determined from strings alone. Maybe a method on IGitHubUrlProvider.
     }
 }
