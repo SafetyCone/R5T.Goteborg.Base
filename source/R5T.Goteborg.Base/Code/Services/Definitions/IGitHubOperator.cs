6ca7a7a3f1e416a1e2098505bd0ac394fecaffa5
MinexAutomation <minex.automation@gmail.com>
2019-12-14 04:25:01 -0800

    Changed CreateRepository() to an asynchronous method returning a repository ID (long).

--- source/R5T.Goteborg.Base/Code/Services/Definitions/IGitHubOperator.cs
+++ source/R5T.Goteborg.Base/Code/Services/Definitions/IGitHubOperator.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 
 namespace R5T.Goteborg
 {
     public interface IGitHubOperator
     {
-        void CreateRepository(GitHubRepository repository);
+        Task<long> CreateRepository(GitHubRepository repository);
         //string GetRepositoryUrl(string repositoryName); // This does not require a GitHub! This can be determined from strings alone. Maybe a method on IGitHubUrlProvider.
     }
 }
