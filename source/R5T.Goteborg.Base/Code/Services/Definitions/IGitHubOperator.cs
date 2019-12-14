65809e7d323f17a811f3ee49de7585091a67b02e
MinexAutomation <minex.automation@gmail.com>
2019-12-14 13:08:42 -0800

    Added GetRepositoryCheckoutUrl() methods.

--- source/R5T.Goteborg.Base/Code/Services/Definitions/IGitHubOperator.cs
+++ source/R5T.Goteborg.Base/Code/Services/Definitions/IGitHubOperator.cs
@@ -10,5 +10,7 @@ namespace R5T.Goteborg
         //string GetRepositoryUrl(string repositoryName); // This does not require a GitHub! This can be determined from strings alone. Maybe a method on IGitHubUrlProvider.
         Task<bool> RepositoryExists(string owner, string name);
         Task DeleteRepository(string owner, string name);
+        Task<string> GetRepositoryCheckoutUrl(string owner, string name);
+        Task<string> GetRepositoryCheckoutUrl(long repositoryID);
     }
 }
